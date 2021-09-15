779b033ceb092b0583041a5f3420571eb6099400
fabiowew <68655289+fabiowew@users.noreply.github.com>
2021-09-14 23:14:55 -0300

    Criação do Projeto

---
+++ pos-java-jdbc/src/main/java/dao/UserPosDAO.java
@@ -0,0 +1,153 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.text.DefaultEditorKit.InsertBreakAction;
+
+import org.junit.Test;
+
+import conexaojdbc.SingleConnection;
+import model.Telefone;
+import model.Userposjava;
+
+public class UserPosDAO {
+
+	@SuppressWarnings("unused")
+	private Connection connection;
+
+	public UserPosDAO() {
+		connection = SingleConnection.getConnection();
+
+	}
+
+	public void saltarTelefone(Telefone telefone) {
+		try {
+			String sql = "INSERT INTO telefoneuser(numero, tipo, usuariopessoa)VALUES (?, ?, ?);";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, telefone.getNumero());
+			statement.setString(2, telefone.getTipo());
+			statement.setLong(3, telefone.getUsuario());
+			statement.execute();
+			connection.commit();
+
+		} catch (Exception e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+
+	}
+
+	@SuppressWarnings("unused")
+	public void salvar(Userposjava userposjava) {
+
+		try {
+			String sql = "insert into userposjava (nome, email) values (?,?)";
+			PreparedStatement insert = connection.prepareStatement(sql);
+			insert.setString(1, userposjava.getNome());
+			insert.setString(2, userposjava.getEmail());
+			insert.execute();
+			connection.commit();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+
+	}
+
+	public List<Userposjava> listar() throws SQLException {
+		List<Userposjava> list = new ArrayList<Userposjava>();
+		String sql = "select * from userposjava";
+
+		PreparedStatement statement = connection.prepareStatement(sql);
+		ResultSet resultado = statement.executeQuery();
+
+		while (resultado.next()) {
+			Userposjava userposjava = new Userposjava();
+			userposjava.setId(resultado.getLong("id"));
+			userposjava.setNome(resultado.getString("nome"));
+			userposjava.setEmail(resultado.getString("email"));
+
+			list.add(userposjava);
+		}
+		return list;
+	}
+
+	public Userposjava buscar(Long id) throws SQLException {
+		Userposjava retorno = new Userposjava();
+
+		String sql = "select * from userposjava where id = " + id;
+
+		PreparedStatement statement = connection.prepareStatement(sql);
+		ResultSet resultado = statement.executeQuery();
+
+		while (resultado.next()) {// aqui ele retorna apenas 1 ou nenhum
+
+			retorno.setId(resultado.getLong("id"));
+			retorno.setNome(resultado.getString("nome"));
+			retorno.setEmail(resultado.getString("email"));
+
+		}
+		return retorno;
+	}
+
+	@Test
+	public void atualizar(Userposjava userposjava) {
+		try {
+			String sql = "update userposjava set nome = ? where id = " + userposjava.getId();
+
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, userposjava.getNome());
+
+			statement.execute();
+			connection.commit();
+
+		} catch (SQLException e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+
+	}
+
+	public void deletar(Long id) {
+		try {
+
+			String sql = "delete from userposjava where id = " + id;
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.execute();
+			connection.commit();
+		} catch (Exception e) {
+			// TODO: handl
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ pos-java-jdbc/src/main/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -0,0 +1,105 @@
+package pos_java_jdbc.pos_java_jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.Test;
+
+import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Telefone;
+import model.Userposjava;
+
+public class TesteBancoJdbc {
+
+	@Test
+	public void initBanco() {
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+
+		userposjava.setNome("Paulo");
+		userposjava.setEmail("paulo@gmail.com");
+
+		userPosDAO.salvar(userposjava);
+
+	}
+
+	@Test
+	public void initListar() {
+		UserPosDAO dao = new UserPosDAO();
+		try {
+			List<Userposjava> list = dao.listar();
+
+			for (Userposjava userposjava : list) {
+				System.out.println(userposjava);
+				System.out.println("*************************************");
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void initBuscar() {
+		UserPosDAO dao = new UserPosDAO();
+
+		try {
+			Userposjava userposjava = dao.buscar(4L);
+			System.out.println(userposjava);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void initAtualizar() {
+		try {
+
+			UserPosDAO dao = new UserPosDAO();
+			Userposjava objetoBanco = dao.buscar(3L);
+
+			objetoBanco.setNome("Rodolfo");
+
+			dao.atualizar(objetoBanco);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void initDeletar() {
+		try {
+			UserPosDAO dao = new UserPosDAO();
+			dao.deletar(4L);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	public void testeInsertTelefone() {
+		Telefone telefone = new Telefone();
+		telefone.setNumero("(41) 9 88886666");
+		telefone.setTipo("casa");
+		telefone.setUsuario(7L);
+		
+		UserPosDAO dao = new UserPosDAO();
+		dao.saltarTelefone(telefone);
+		
+		
+	}
+
+}
